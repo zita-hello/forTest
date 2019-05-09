44ac046016344f532c58c14eaf68e81fa2cc43b0
zita <c_zhuoxuan@163.com>
2019-05-09 17:53:46 +0800

    forTest

---
+++ WorkspaceforTest/e3-manager/e3-manager-pojo/src/main/java/e3/mall/App.java
@@ -0,0 +1,13 @@
+package e3.mall;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
